3730e3f96dc55120fc0b01422982621e78dedd84
Dipesh1203 <prajapatdipesh86@gmail.com>
2025-08-11 01:56:19 +0530

    feat:final production configurations

--- apps/scribbledraw-frontend/next.config.ts
+++ apps/scribbledraw-frontend/next.config.ts
@@ -6,6 +6,12 @@ const nextConfig: NextConfig = {
     config.externals = [...config.externals, { canvas: "canvas" }];
     return config;
   },
+  env: {
+    NEXT_PUBLIC_BACKEND_URL: process.env.BACKEND_URL || "http://localhost:3000",
+    NEXT_PUBLIC_WS_URL: process.env.WS_URL || "ws://localhost:3002",
+    NEXT_PUBLIC_FRONTEND_URL:
+      process.env.FRONTEND_URL || "http://localhost:3003",
+  },
 };
 
 export default nextConfig;
